863f6a67eb4aea2079faec11b309817d96c7db76
Exsedow <endybjb22@gmail.com>
2024-06-05 16:46:56 +0800

    membuat class MetematikaBeraksi

--- src/hitung/MatematikaBeraksi.java
+++ src/hitung/MatematikaBeraksi.java
@@ -9,5 +9,13 @@ package hitung;
  * @author USER
  */
 public class MatematikaBeraksi {
-    
+    public static void main(String[] args) {
+        Matematika nando = new Matematika(9,1);
+        
+        System.out.println("Hasil Penjumlahan: " + nando.setPenjumlahan());
+        System.out.println("Hasil Pengurangan: " + nando.setPengurangan());
+        System.out.println("Hasil Perkalian: " + nando.setPerkalian());
+        System.out.println("Hasil Pembagian: " + nando.setPembagian());
+    }
+
 }
